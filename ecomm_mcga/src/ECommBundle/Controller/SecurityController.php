40ec5c3cc42409f9eb05aab6e364ba3a213299ba
LeGrandMaxime <rischebourg.e1601054@etud.univ-ubs.fr>
2017-09-18 09:42:16 +0200

    ajout user

--- ecomm_mcga/src/ECommBundle/Controller/SecurityController.php
+++ ecomm_mcga/src/ECommBundle/Controller/SecurityController.php
@@ -4,35 +4,74 @@ namespace ECommBundle\Controller;
 
 
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+#use for register
+use ECommBundle\Form\UserType;
+use ECommBundle\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 class SecurityController extends Controller
 {
-    
     /**
     * @Route("/login", name="login")
     */
     public function loginAction(Request $request)
     {
+      $authUtils = $this->get('security.authentication_utils');
+      // get the login error if there is one
+      $error = $authUtils->getLastAuthenticationError();
+      // last username entered by the user
+      $lastUsername = $authUtils->getLastUsername();
+      return $this->render('ECommBundle:Security:login.html.twig', array(
+        'last_username' => $lastUsername,
+        'error'         => $error,
+      ));
+     }
+
+
+
+     /**
+     * @Route("/register", name="user_registration")
+     */
+     public function registerAction(Request $request)
+     {
+        $passwordEncoder = $this->get('security.password_encoder');
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('page_home');
+        }
+
+        return $this->render(
+            'ECommBundle:Security:register.html.twig',
+            array('form' => $form->createView())
+        );
     }
 
-    /**
-    * @Route("/logout", name="logout")
-    */
-    public function logoutAction(Request $request)
-    {
-    }
-    /**
-    * @Route("/register", name="register")
-    */
-    public function registerAction(Request $request)
-    {
-    //creation nouvel utilisateur
-    }
 
-    /**
-    * @Route("/profile", name="register")
-    */
-    public function profileAction(Request $request)
-    {
-    }
-    
+
+
+
 }
---
+++ ecomm_mcga/src/ECommBundle/Entity/User.php
@@ -0,0 +1,165 @@
+<?php
+// src/ECommBundle/Entity/User.php
+namespace ECommBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Table(name="app_users")
+ * @ORM\Entity(repositoryClass="ECommBundle\Repository\UserRepository")
+ * @UniqueEntity(fields="email", message="Email already taken")
+ * @UniqueEntity(fields="username", message="Username already taken")
+ */
+class User implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $username;
+
+    /**
+     * The below length depends on the "algorithm" you use for encoding
+     * the password, but this works well with bcrypt.
+     *
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        // may not be needed, see section on salt below
+        // $this->salt = md5(uniqid('', true));
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
+    }
+
+
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPassword($password)
+   {
+       $this->password = $password;
+   }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function eraseCredentials()
+    {
+    }
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized);
+    }
+
+}
