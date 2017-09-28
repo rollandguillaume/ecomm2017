e811a89f6f6c3221e185762f2865e122e757b79b
guillaumerolland <guillaume.rolland21@gmail.com>
2017-09-28 12:01:20 +0200

    correction commandesrepository

--- ecomm_mcga/src/ECommBundle/DataFixtures/ORM/ProduitsData.php
+++ ecomm_mcga/src/ECommBundle/DataFixtures/ORM/ProduitsData.php
@@ -12,28 +12,28 @@ class ProduitsData extends AbstractFixture implements OrderedFixtureInterface
     {
         $produit1 = new Produits();
         $produit1->setDescription("excelent cidre");
-        $produit1->setDisponible('1');
+        // $produit1->setDisponible('1');
         $produit1->setNom('cidre');
         $produit1->setPrix('1.99');
         $manager->persist($produit1);
-        
+
         $produit2 = new Produits();
         $produit2->setDescription("excelent vin");
-        $produit2->setDisponible('1');
+        // $produit2->setDisponible('1');
         $produit2->setNom('vin');
         $produit2->setPrix('1.99');
         $manager->persist($produit2);
 
         $produit3 = new Produits();
         $produit3->setDescription("excelent vin");
-        $produit3->setDisponible('1');
+        // $produit3->setDisponible('1');
         $produit3->setNom('vin2');
         $produit3->setPrix('1.99');
         $manager->persist($produit3);
-        
+
         $manager->flush();
     }
-    
+
     public function getOrder()
     {
         return 4;
