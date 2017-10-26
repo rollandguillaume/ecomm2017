b57f141242a54c401204a28bcf298a760334da52
guillaumerolland <guillaume.rolland21@gmail.com>
2017-10-26 11:36:53 +0200

    debut send mail

--- ecomm_mcga/src/ECommBundle/Controller/PageController.php
+++ ecomm_mcga/src/ECommBundle/Controller/PageController.php
@@ -13,4 +13,36 @@ class PageController extends Controller
   {
     return $this->render('ECommBundle:Page:index.html.twig');
   }
+
+  public function contactAction(Request $request) {
+
+    $post = $request->request;
+    if (!(($post->get('subject') === null) || ($post->get('contenu') === null))) {
+
+      $sujet = $post->get('subject');
+      $contenu = $post->get('contenu');
+
+      if ((strlen($sujet)>=5) && (strlen($contenu)>=50)) {
+        $message = (new \Swift_Message($sujet))
+        ->setFrom('send@example.com')
+        ->setTo('recipient@example.com')
+        ->setBody(
+          $this->renderView(
+            // app/Resources/views/Emails/registration.html.twig
+            'Emails/contactMail.html.twig',
+            array(
+              'contenu' => $contenu,
+            )
+          ),
+          'text/html'
+          )
+          ;
+          $this->get('mailer')->send($message);
+      }
+
+    }
+
+    return $this->render('ECommBundle:Page:contact.html.twig');
+  }
+
 }
